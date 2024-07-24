504ab30f6430e7996110e8f0a3d027b62e88ac65
Nick <nicolasdaniel2106@gmail.com>
2024-07-24 15:37:04 -0400

    outras mudanças

--- sintaxe/tipos-de-dados/src/Main.java
+++ sintaxe/tipos-de-dados/src/Main.java
@@ -1,29 +1,29 @@
 public class Main {
-    public static void main(String[] args) {
-        var tv = new SmartTV();
-
-        System.out.println("\nINFOS ----------");
-        System.out.printf("\tEstado: %s\n", tv.ligada ? "ligada" : "desligada");
-        System.out.printf("\tCanal atual: %d\n", tv.canal);
-        System.out.printf("\tVolume atual: %d\n", tv.volume);
-
-        tv.desligar();
-        tv.diminuirVolume();
-        tv.diminuirVolume();
-        tv.mudarCanal();
-
-        System.out.println("\nINFOS ----------");
-        System.out.printf("\tEstado: %s\n", tv.ligada ? "ligada" : "desligada");
-        System.out.printf("\tCanal atual: %d\n", tv.canal);
-        System.out.printf("\tVolume atual: %d\n", tv.volume);
-
-        tv.mudarCanal(24);
-        tv.ligar();
-
-        System.out.println("\nINFOS ----------");
-        System.out.printf("\tEstado: %s\n", tv.ligada ? "ligada" : "desligada");
-        System.out.printf("\tCanal atual: %d\n", tv.canal);
-        System.out.printf("\tVolume atual: %d\n", tv.volume);
-
-    }
+//    public static void main(String[] args) {
+//        var tv = new SmartTV();
+//
+//        System.out.println("\nINFOS ----------");
+//        System.out.printf("\tEstado: %s\n", tv.ligada ? "ligada" : "desligada");
+//        System.out.printf("\tCanal atual: %d\n", tv.canal);
+//        System.out.printf("\tVolume atual: %d\n", tv.volume);
+//
+//        tv.desligar();
+//        tv.diminuirVolume();
+//        tv.diminuirVolume();
+//        tv.mudarCanal();
+//
+//        System.out.println("\nINFOS ----------");
+//        System.out.printf("\tEstado: %s\n", tv.ligada ? "ligada" : "desligada");
+//        System.out.printf("\tCanal atual: %d\n", tv.canal);
+//        System.out.printf("\tVolume atual: %d\n", tv.volume);
+//
+//        tv.mudarCanal(24);
+//        tv.ligar();
+//
+//        System.out.println("\nINFOS ----------");
+//        System.out.printf("\tEstado: %s\n", tv.ligada ? "ligada" : "desligada");
+//        System.out.printf("\tCanal atual: %d\n", tv.canal);
+//        System.out.printf("\tVolume atual: %d\n", tv.volume);
+//
+//    }
 }
--- sintaxe/tipos-de-dados/src/SmartTV.java
+++ sintaxe/tipos-de-dados/src/SmartTV.java
@@ -28,4 +28,5 @@ public class SmartTV {
     public void mudarCanal(int novoCanal) {
         canal = novoCanal;
     }
+
 }
