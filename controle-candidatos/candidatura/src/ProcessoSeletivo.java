b2b2cbb91e96de8e075ddc2835f651ce764707ca
Nick <nicolasdaniel2106@gmail.com>
2024-07-26 12:28:12 -0400

    adicionando método imprimirSelecionados e ajustando o código

--- controle-candidatos/candidatura/src/ProcessoSeletivo.java
+++ controle-candidatos/candidatura/src/ProcessoSeletivo.java
@@ -2,38 +2,11 @@ import java.util.Scanner;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        double salarioBase = 2000;
-        double salarioPretendido;
-
         String[] candidatos = {"Felipe", "Marcia", "Jose", "Joao", "Augusta", "Luiz", "Rafael", "Higor", "Heitor", "Paloma", "Guilherme"};
-        String[] candidatosSelecionados = new String[5];
-        var scanner = new Scanner(System.in);
 
-        int count = 0;
-        for (int i = 0; i < candidatos.length && count < 5; i++) {
-            System.out.print("Informe o salário pretendido: ");
-            salarioPretendido = scanner.nextDouble();
-
-            if (analisarCandidato(salarioBase, salarioPretendido)) {
-                candidatosSelecionados[count] = candidatos[i];
-                count++;
-            }
-        }
-
-        System.out.println("CANDIDATOS SELECIONADOS --------------");
-        boolean flag = false;
-
-        for (String candidato : candidatosSelecionados) {
-            if (candidato != null) {
-                System.out.println("\t" + candidato);
-                flag = true;
-            }
-        }
-
-        if (!flag) {
-            System.out.println("\tNenhum candidato foi selecionado \uD83D\uDE41");
-        }
+        imprimirSelecionados(selecionarCandidatos(candidatos, 2000));
     }
+
 
     //CASE 1
     static boolean analisarCandidato(double salarioBase, double salarioPretendido) {
@@ -46,6 +19,42 @@ public class ProcessoSeletivo {
         } else {
             System.out.println("AGUARDANDO RESULTADO DOS DEMAIS CANDIDATOS...\n");
             return false;
+        }
+    }
+
+    //CASE 2
+    static String[] selecionarCandidatos(String[] candidatos, double salarioBase) {
+        String[] candidatosSelecionados = new String[5];
+        var scanner = new Scanner(System.in);
+
+        int count = 0;
+        for (int i = 0; i < candidatos.length && count < 5; i++) {
+            System.out.print("Informe o salário pretendido: ");
+            double salarioPretendido = scanner.nextDouble();
+
+            if (analisarCandidato(salarioBase, salarioPretendido)) {
+                candidatosSelecionados[count] = candidatos[i];
+                count++;
+            }
+        }
+
+        return candidatosSelecionados;
+    }
+
+    //CASE 3
+    static void imprimirSelecionados(String[] candidatosSelecionados) {
+        System.out.println("CANDIDATOS SELECIONADOS --------------");
+        boolean flag = false;
+
+        for (String candidato : candidatosSelecionados) {
+            if (candidato != null) {
+                System.out.println("\t" + candidato);
+                flag = true;
+            }
+        }
+
+        if (!flag) {
+            System.out.println("\tNenhum candidato foi selecionado \uD83D\uDE41");
         }
     }
 }
